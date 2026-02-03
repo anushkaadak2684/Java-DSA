4679ead8d68a6a3015ac46597de893e911118e10
anushkaadak2684 <anushkaadakclassx@gmail.com>
2026-02-04 00:17:50 +0530

    Added Sorting

---
+++ Sorting/CountSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class CountSort{
+    public static void countSort(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0; i<arr.length; i++){
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest+1];
+        for(int i=0; i<arr.length; i++){
+            count[arr[i]]++;
+        }
+        int j = 0;
+        for(int i=0; i<count.length; i++){
+            while(count[i] > 0){
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
+        System.out.print("Sorted array: ");
+        for(int i=0; i<arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[] = new int[5];
+        System.out.print("Enter an array of numbers: ");
+        for(int i=0; i<arr.length; i++)
+            arr[i] = in.nextInt();
+        countSort(arr);
+        in.close();
+    }
+}
